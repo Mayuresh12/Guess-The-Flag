0aa97ce03d149ddd61c2081f4e76edf7a59c760a
MRAO0212 <mayureshvrao@gmail.com>
2020-08-09 20:49:34 -0700

    Creation of the Project

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Mayuresh Rao on 8/9/20.
+//  Copyright © 2020 Mayuresh Rao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Text("Hello, World!")
+            Text("Hello, World!")
+            Text("Hello, World!")
+            Text("Hello, World!")
+            
+        }
+        
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
