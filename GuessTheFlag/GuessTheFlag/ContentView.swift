c7cda81bc56ac4377eb5881025691b5f753890a3
MRAO0212 <mayureshvrao@gmail.com>
2020-08-20 02:33:38 -0700

    added flags and UI for the first screen

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,15 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
     var body: some View {
-        Button(action: {
-            print("Button is tapped")
-        }) {
-            Text("Tap me!")
-            Image(systemName: "pencil").renderingMode(.original)
+        ZStack{
+            Color.blue.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30){
+                VStack{
+                    Text("Tap the flag of").foregroundColor(.white)
+                    Text(self.countries[correctAnswer]).foregroundColor(.white)
+                    
+                    ForEach(0..<3) {
+                        number in Button(action: {} ) {
+                            Image(self.countries[number]).renderingMode(.original)
+                        }
+                    }
+                    Spacer()
+                }
+            }
         }
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
